964245f084017d216c5cfc967be3dbea07c3cdb0
Dmitry <enindmitry8@gmail.com>
2022-08-11 23:44:01 +1000

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+import React from 'react';
+
+
+class App extends React.Component {
+  render(){
+    return (
+    <div className="App">
+      <h1>ravenous</h1>
+      <SearchBar />
+      <BusinessList /> 
+    </div>
+  );}
+}
+
+ 
+export default App;
